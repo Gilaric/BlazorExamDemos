ae6652e3d5d0a4f8d538b8e1e96bd6a5581217d2
Gilaric <47851759+Gilaric@users.noreply.github.com>
2022-11-10 11:58:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Overloading_demo1
+{
+    internal class Program
+    {
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            // Samme metode navn, men kan gøre noget forskelligt. 
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+}
